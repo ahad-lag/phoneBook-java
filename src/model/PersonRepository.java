72f1ea8ec8ca439ea4020f8e8b356ab56376994c
ahad-lag <mr.ahad.lag@gmail.com>
2022-12-16 16:44:32 +0330

    create tableNames and personRepository

---
+++ src/model/PersonRepository.java
@@ -0,0 +1,55 @@
+package model;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PersonRepository {
+    private Connection connection;
+
+    public PersonRepository(Connection connection) {
+        this.connection = connection;
+    }
+
+    public List<Person> getList(){
+        Statement statement = null;
+        ResultSet results = null;
+
+        try {
+            statement = this.connection.createStatement();
+            results = statement.executeQuery("SELECT * From " + TableNames.TABLE_PEOPLE);
+            List<Person> People = new ArrayList<>();
+
+            while (results.next()) {
+                Person person = new Person();
+                person.setId(results.getInt(TableNames.COLUMN_PEOPLE_ID));
+                person.setName(results.getString(TableNames.COLUMN_PEOPLE_NAME));
+                person.setFamily(results.getString(TableNames.COLUMN_PEOPLE_FAMILY));
+                People.add(person);
+            }
+            return People;
+
+        } catch (SQLException e) {
+            System.out.println("Error in Execute Query " + e.getMessage());
+            return null;
+        } finally {
+            try {
+                if (results != null) {
+                    results.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                if (statement != null) {
+                    statement.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/model/TableNames.java
@@ -0,0 +1,15 @@
+package model;
+
+public class TableNames {
+    public static final String TABLE_PEOPLE="People";
+
+    public static final String COLUMN_PEOPLE_ID="Id";
+    public static final String COLUMN_PEOPLE_NAME="Name";
+    public static final String COLUMN_PEOPLE_FAMILY="Family";
+
+    public static final String TABLE_NUMBERS="Numbers";
+
+    public static final String COLUMN_NUMBERS_ID="Id";
+    public static final String COLUMN_NUMBERS_PERSONID="PersonId";
+    public static final String COLUMN_NUMBERS_NUMBER="Number";
+}
